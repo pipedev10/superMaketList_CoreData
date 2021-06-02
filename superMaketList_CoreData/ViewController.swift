7b074ed0ebf00ab3469a6cd35cb1d25afeeb5e82
Felipe Carrasco <felipe@bakers.dev>
2021-06-01 21:40:10 -0400

    Initial Commit

---
+++ superMaketList_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  superMaketList_CoreData
+//
+//  Created by Pipe Carrasco on 01-06-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
